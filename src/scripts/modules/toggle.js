a7f9b00f6b0007ec1f8ae681ea4e412028623d26
DmitryRybalko <dmitryrybalkodev@gmail.com>
2021-12-18 15:02:52 +0800

    style accordions on wide screens

--- src/scripts/modules/toggle.js
+++ src/scripts/modules/toggle.js
@@ -2,9 +2,17 @@
 
 export const toggleCard = () => {
   const accordions = document.querySelectorAll(".main__accordion-content");
+  const accordionItems = document.querySelectorAll(".main__accordion-item");
   const arrowBtns = document.querySelectorAll(".main__accordion-toggle");
   const arrowIcons = document.querySelectorAll(".main__accordion-toggle-icon");
   const cards = document.querySelectorAll(".main__accordion-content");
+  const textsOnHover = document.querySelectorAll(".main__text--hover");
+  const show = (i) => {
+    textsOnHover[i].style.maxHeight = textsOnHover[i].scrollHeight + "px";
+  };
+  const hide = (i) => {
+    textsOnHover[i].style.maxHeight = 0;
+  };
   arrowBtns.forEach((arrowBtn, i) => {
     arrowBtn.addEventListener("click", () => {
       arrowIcons[i].classList.toggle("icon--rotate");
@@ -15,5 +23,11 @@ export const toggleCard = () => {
         accordions[i].style.maxHeight = 0;
       }
     });
+  });
+  accordionItems.forEach((accordionItem, i) => {
+    accordionItem.addEventListener("mouseenter", () => show(i));
+    accordionItem.addEventListener("focus", () => show(i));
+    accordionItem.addEventListener("mouseleave", () => hide(i));
+    accordionItem.addEventListener("focusout", () => hide(i));
   });
 };
