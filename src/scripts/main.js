08206a8234ee4fe43fd949703cbe1c0dec91b5de
DmitryRybalko <dmitryrybalkodev@gmail.com>
2021-12-19 19:29:43 +0800

    create a section with an apartment choice

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@ import { toggleMenu } from "./modules/menu";
 import { toggleNavigation } from "./modules/navigation";
 import { toggleCard } from "./modules/toggle";
 
-toggleNavigation();
-toggleMenu();
-toggleCard();
+document.addEventListener("DOMContentLoaded", function () {
+  toggleNavigation();
+  toggleMenu();
+  toggleCard();
+});
