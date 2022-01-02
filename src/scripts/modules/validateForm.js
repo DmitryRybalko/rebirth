12ef8a47c78b9da6872b52c4b574c4eed590b87d
DmitryRybalko <dmitryrybalkodev@gmail.com>
2022-01-02 16:34:41 +0800

    validate form

--- src/scripts/modules/validateForm.js
+++ src/scripts/modules/validateForm.js
@@ -8,14 +8,21 @@ export const validateForm = () => {
   const mask = IMask(element, maskOptions);
   mask.updateValue();
 
+  const modalForm = document.querySelector(".modal__form");
   const submitBtn = document.querySelector(".modal__btn");
   const inputs = document.querySelectorAll(".modal__input");
   const labels = document.querySelectorAll(".modal__label");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   submitBtn.addEventListener("click", () => {
     inputs.forEach((input, i) => {
       if (inputs[i].value.length <= 0) {
         inputs[i].classList.add("validate-input");
         labels[i].classList.add("validate-label");
+        modalForm.addEventListener("submit", handleSubmit);
       } else {
         inputs[i].classList.remove("validate-input");
         labels[i].classList.remove("validate-label");
